0ce374def1f8f15b2d0b414fba2b8135a8af3aad
피수영 <vltndud123@naver.com>
2024-06-10 04:06:23 +0900

    chore: Set build.gradle contains dependencies, jib image build, annotated options.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,17 @@ plugins {
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.5"
     id("org.jetbrains.kotlin.plugin.noarg") version "1.4.32"
+    id("com.google.cloud.tools.jib") version "3.4.1"
     kotlin("plugin.jpa") version "1.9.24"
+    kotlin("kapt") version "1.9.24"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
 }
 
 group = "org.example"
 version = "0.0.1-SNAPSHOT"
+
+val queryDslVersion = "5.0.0"
 
 java {
     toolchain {
@@ -20,6 +24,12 @@ repositories {
     mavenCentral()
 }
 
+// Classes annotated with the below annotations will automatically have a no-argument constructor generated.
+noArg {
+    annotation("jakarta.persistence.Entity")
+}
+
+// Classes annotated with the below annotations are marked as 'open' to allow inheritance.
 allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.Embeddable")
@@ -31,10 +41,43 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0") // Use for swaggerUI.
+
+    implementation("com.querydsl:querydsl-jpa:${queryDslVersion}:jakarta")
+    kapt("com.querydsl:querydsl-apt:${queryDslVersion}:jakarta") // Use for QueryDsl.
+
     runtimeOnly("com.h2database:h2")
+
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.mockk:mockk:1.13.10") // Use for mocking instances in tests.
+    testImplementation("com.appmattus.fixture:fixture:1.2.0") // Use for simplifying the creation of fixtures in tests.
+}
+
+jib {
+    val profile: String = findProperty("profile")?.toString() ?: "local"
+    val tag: String = findProperty("tag")?.toString() ?: "0.0.1"
+
+    from {
+        image = ("openjdk:17-alpine")
+    }
+    to {
+        image = "nutrition-components"
+        tags = setOf("latest-$profile", tag)
+    }
+    container {
+        creationTime.set("USE_CURRENT_TIMESTAMP")
+        ports = listOf("8080")
+        jvmFlags =
+            listOf(
+                "-Dspring.profiles.active=$profile",
+                "-XX:+UseContainerSupport",
+                "-Dserver.port=8080",
+                "-Dfile.encoding=UTF-8",
+            )
+    }
 }
 
 kotlin {
