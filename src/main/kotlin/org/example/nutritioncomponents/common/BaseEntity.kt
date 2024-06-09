e815f83bb50e32f4c1617c6d8d5d3538778eb867
피수영 <vltndud123@naver.com>
2024-06-10 04:03:00 +0900

    chore: make BaseEntity class.

--- src/main/kotlin/org/example/nutritioncomponents/common/BaseEntity.kt
+++ src/main/kotlin/org/example/nutritioncomponents/common/BaseEntity.kt
@@ -1,7 +1,6 @@
 package org.example.nutritioncomponents.common
 
-import jakarta.persistence.EntityListeners
-import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
@@ -10,6 +9,8 @@ import java.time.LocalDateTime
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
 class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
     @CreatedDate
     var createdAt: LocalDateTime? = null
